d750bfd1b2031a2a9fdf14cee925eade4ccc1084
Lisa S <halisasaipulla@gmail.com>
2023-07-10 07:09:06 -0700

    Adding a leetcode easy solution

---
+++ Majority_Element.py
@@ -0,0 +1,46 @@
+"""
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. 
+You may assume that the majority element always exists in the array.
+
+Example 1:
+Input: nums = [3,2,3]
+Output: 3
+
+Example 2:
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+
+Constraints:
+n == nums.length
+1 <= n <= 5 * 104
+-109 <= nums[i] <= 109
+
+"""
+
+# Time complexity: O(n)
+# Space complexity: O(1)
+
+def majorityElement(nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count = 0
+        candidate = None
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+
+            if num == candidate:
+                count += 1
+            else:
+                count -= 1
+
+        return candidate
+
+print(majorityElement([3,2,3])) #3
+print(majorityElement([2,2,1,1,1,2,2])) #2
+
--- Remove_Duplicates_from_Sorted_Array.py
+++ Remove_Duplicates_from_Sorted_Array.py
@@ -40,5 +40,5 @@ def removeDuplicates(self, nums):
     for i in range(1, n):
         if nums[i-1] != nums[i]:
             nums[j] = nums[i]
-            j = j  +1
+            j = j + 1
     return j

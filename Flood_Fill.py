3094975058f5b365dfebfa786cca9004b2e5c950
Lisa S <halisasaipulla@gmail.com>
2023-11-15 13:37:13 -0800

    Adding a leetcode easy solution

---
+++ Flood_Fill.py
@@ -0,0 +1,69 @@
+"""
+An image is represented by an m x n integer grid image where image[i][j] represents the pixel value of the image.
+
+You are also given three integers sr, sc, and color. You should perform a flood fill on the image starting from the pixel image[sr][sc].
+
+To perform a flood fill, consider the starting pixel, plus any pixels connected 4-directionally to the starting pixel of the same color as the starting pixel, plus any pixels connected 4-directionally to those pixels (also with the same color), 
+and so on. Replace the color of all of the aforementioned pixels with color.
+
+Return the modified image after performing the flood fill.
+
+Example :
+Input: image = [[1,1,1],[1,1,0],[1,0,1]], sr = 1, sc = 1, color = 2
+Output: [[2,2,2],[2,2,0],[2,0,1]]
+Explanation: From the center of the image with position (sr, sc) = (1, 1) (i.e., the red pixel), all pixels connected by a path of the same color as the starting pixel (i.e., the blue pixels) are colored with the new color.
+Note the bottom corner is not colored 2, because it is not 4-directionally connected to the starting pixel.
+
+Example 2:
+Input: image = [[0,0,0],[0,0,0]], sr = 0, sc = 0, color = 0
+Output: [[0,0,0],[0,0,0]]
+Explanation: The starting pixel is already colored 0, so no changes are made to the image.
+
+Constraints:
+
+m == image.length
+n == image[i].length
+1 <= m, n <= 50
+0 <= image[i][j], color < 216
+0 <= sr < m
+0 <= sc < n
+"""
+# Time complexity: O(n)
+# Space complexity: O(n)
+def floodFill(image, sr, sc, color):
+    """
+    :type image: List[List[int]]
+    :type sr: int
+    :type sc: int
+    :type color: int
+    :rtype: List[List[int]]
+    """
+    R = len(image)
+    C = len(image[0])
+    src_color = image[sr][sc]
+
+    if src_color==color:
+        return image
+
+    def dfs( r, c):
+        if image[r][c]== src_color:
+            image[r][c]=color
+
+            if r>=1:
+                dfs(r-1,c)
+
+            if c>=1:
+                dfs(r,c-1)
+
+            if r+1<R:
+                dfs(r+1,c)
+
+            if c+1<C:
+                dfs(r,c+1)
+
+    dfs(sr,sc)
+    return image
+
+print(floodFill([[1,1,1],[1,1,0],[1,0,1]], 1, 1, 2))
+print(floodFill([[0,0,0],[0,0,0]], 0, 0, 0))
+
